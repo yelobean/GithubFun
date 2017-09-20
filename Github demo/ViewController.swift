f2f36e819fd9b1d85401006df33b4b53e7d5413f
DUHUN HWANG <yelobean@DUHUNui-MacBook-Pro.local>
2017-09-20 23:26:38 +0900

    red

--- Github demo/ViewController.swift
+++ Github demo/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        view.backgroundColor = UIColor.yellow
+        view.backgroundColor = UIColor.red
     }
 
     override func didReceiveMemoryWarning() {
